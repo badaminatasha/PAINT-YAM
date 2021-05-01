6efb5d6b1d41a29209739acb49f0e76c77a4a009
Natasha Badami <nbadami@umich.edu>
2021-05-01 00:14:46 -0400

    updated 2d animation packages, made broccoli cheddar idle animation

---
+++ Assets/Scripts/AttackSequence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSequence : MonoBehaviour
+{
+    [SerializeField] List<Attack> attackSequence = new List<Attack>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSequence());
+    }
+
+    IEnumerator StartSequence()
+    {
+        while (true)
+        {
+            foreach (Attack attack in attackSequence)
+            {
+                yield return attack.DoAttack();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TEST_ATTACK.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_ATTACK : Attack
+{
+    public override IEnumerator DoAttack()
+    {
+        Debug.Log("TEST ATTACK START");
+        yield return new WaitForSeconds(delayBeforeStart);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
